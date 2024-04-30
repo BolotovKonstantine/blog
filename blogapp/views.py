8fc8f51d35c4f5bec0d09f193b87f54cbabce604
BolotovKonstantine <bolotoff@byui.edu>
2024-04-30 23:26:19 +0300

    update view finished

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+
 from .models import Post
 # Create your views here.
 
@@ -19,3 +20,8 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = [ 'title', 'body']
